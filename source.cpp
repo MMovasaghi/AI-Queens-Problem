9f9edbe60fe781a54159a7b232d3c0de69dc3dff
MH.Movasaghinia <mmghho77@gmail.com>
2020-08-08 13:07:23 +0430

    project done

--- source.cpp
+++ source.cpp
@@ -2,284 +2,172 @@
 #include <ctime>
 #include <chrono>
 #include <cmath>
-#include <stdlib.h> 
+#include <stdlib.h>
+#include<cstdlib>
+#include<algorithm>
+#include<sstream>
+#include<cstring>
+#include<map>
+#include<list>
+#include<queue>
 using namespace std;
 
-#define GENERATION_NUMBER 5000
-#define QUEENS_NUMBER 8
+#define ASCII_NUMBER_SHIFT 48
 
-class queen
+typedef struct
 {
-    public:
-        int x;
-        int y;
-        queen()
-        {
-            x = -1;
-            y = -1;
-        }     
-};
-class genom
-{
-    public:
-        queen queens[8];
-        int checkeds;
-        genom()
-        {
-            checkeds = 0;
-        }
-};
+    string arrangement;
+    int cost;
+} chromosome;
 
-int compare (const void * a, const void * b) // compare function for QuickSort
+typedef vector<chromosome*> genom_type;
+genom_type population;
+long generation;
+int initialPopSize;
+int boardSize;
+int printStep;
+int mutationRate;
+
+int fitnessValue(string arrangement)
 {
-    return ( *(int*)a - *(int*)b );
+    int fitness = (boardSize * (boardSize - 1) )/2;
+    for(int i = 0; i < boardSize; i++)
+        for(int j = i + 1; j < boardSize; j++)
+            if((arrangement[i] == arrangement[j]) || (i-arrangement[i] == j-arrangement[j]) || (i+arrangement[i] == j+arrangement[j]))
+                fitness--;
+    return fitness;
 }
 
-genom generations[GENERATION_NUMBER];
-genom gens[QUEENS_NUMBER][GENERATION_NUMBER];
-int gensLength[QUEENS_NUMBER + 1] = {0};
-int maxProfit = 0;
-
-int numbers[QUEENS_NUMBER] = {0};
-int ptrNumbers = 0;
-int totalNumber = 0;
-
-int mutationRate = 5;
-
-int generation = 0;
-
-int crossOverCallNumber = 0;
-
-void printResult(queen queens[])
+chromosome* createGenom()
 {
-    cout << "--------------------------------" << endl;
-    cout << "Result #" << generation << endl;
-    for (int j = 0; j < QUEENS_NUMBER; j++)
-    {
-        cout << "Q[" << j << "] : [ " << queens[j].x << " , " << queens[j].y << " ]" << endl;
-    }
-}
-
-genom mutation()
-{
-    int x[8] = {0};
-    int y[8] = {0};
-    genom newGenom;
-    for (int j = 0; j < QUEENS_NUMBER; j++)
-    {
-        bool checked = true;
-        int newX;
-        while (checked)
-        {
-            newX = rand() % QUEENS_NUMBER;
-            int k;
-            for (k = 0; k < j; k++)
-            {
-                if (x[k] == newX)
-                {
-                    break;
-                }                    
-            }
-            if(k == j)
-            {
-                checked = false;
-                x[j] = newX;
-            }                           
-        }
-        newGenom.queens[j].x = newX;
-
-        checked = true;
-        int newY;
-        while (checked)
-        {
-            newY = rand() % QUEENS_NUMBER;
-            int k;
-            for (k = 0; k < j; k++)
-            {
-                if (y[k] == newY)
-                {
-                    break;
-                }                    
-            }
-            if(k == j)
-            {
-                checked = false;
-                y[j] = newY;
-            }               
-        }
-        newGenom.queens[j].y = newY;
-    }
+    chromosome *newGenom  = new chromosome;
     return newGenom;
 }
 
-int fitness(queen queens[])
+void generateFirstGeneration()
 {
-    int checkedNumber = 0;
-    for (int i = 0; i < QUEENS_NUMBER; i++)
+    string tmpArrengment = "";
+    chromosome *temp;
+    for(int i=1; i <= boardSize; i++)
     {
-        int j;    
-        for (j = 0; j < QUEENS_NUMBER; j++)
-        {
-            if (i != j)
-            {
-                if (queens[i].x == queens[j].x || queens[i].y == queens[j].y)
-                break;
-                if ((queens[i].x + queens[i].y) == (queens[j].x + queens[j].y) || abs(queens[i].x - queens[i].y) == abs(queens[j].x - queens[j].y))
-                    break;
-            }           
-        }
-        if (j == QUEENS_NUMBER)
-            checkedNumber++;  
+        tmpArrengment += i + ASCII_NUMBER_SHIFT;
     }
-    return checkedNumber;
+
+    // add to population
+    for(int i=0; i < initialPopSize; i++)
+    {
+        random_shuffle( tmpArrengment.begin(), tmpArrengment.end());
+        temp   = createGenom();
+        temp->arrangement = tmpArrengment;
+        temp->cost = fitnessValue(tmpArrengment);
+        population.push_back(temp);
+    }
 }
 
-bool classificationAndCheck(queen queens[])
+chromosome* reproduce(chromosome *x, chromosome *y)
 {
-    int number = fitness(queens);    
-    bool check = true;
-    for (int i = 0; i < QUEENS_NUMBER; i++)
-    {
-        if(gensLength[number] > GENERATION_NUMBER)
-            gensLength[number] = 50;
-        gens[number][gensLength[number]].queens[i] = queens[i];
-        if (numbers[i] == number)
-            check = false;        
-    }    
+    chromosome *child = createGenom();
+    int rnd = rand() % boardSize;
+    // crossOver
+    child->arrangement = (x->arrangement).substr(0, rnd) + (y->arrangement).substr(rnd, boardSize - rnd + 1);
+    child->cost = fitnessValue(child->arrangement);
+    return child;
+}
 
-    gensLength[number]++;
+chromosome* mutate(chromosome *child)
+{
+    int randomR = (rand() % (boardSize)) + 1;
+    int randomC = (rand() % (boardSize)) + 1;
+    child->arrangement[randomR] = randomC + ASCII_NUMBER_SHIFT;
+    return child;
+}
 
-    if(check)
-    {
-        numbers[0] = number;
-        totalNumber += number;
-    }
-    
-    if (number > maxProfit)
-    {
-        maxProfit = number;
-        if (maxProfit == QUEENS_NUMBER)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }        
-    } 
+int randomSelection()
+{
+    int randomC = rand() % population.size() % 2;
+    return randomC;
+}
+
+bool checkFit(chromosome *test)
+{
+    if(fitnessValue(test->arrangement)==((boardSize*(boardSize-1))/2))
+        return true;
     return false;
 }
 
-void generateFirstGeneration() //generator with simple x and y checker
+bool comp(chromosome *a, chromosome*b)
 {
-    for (long i = 0; i < GENERATION_NUMBER; i++)
-    {
-        genom createdGenom = mutation();
-        for (int j = 0; j < QUEENS_NUMBER; j++)
-        {
-            generations[i].queens[j].x = createdGenom.queens[j].x;
-            generations[i].queens[j].y = createdGenom.queens[j].y;
-        }
-        if(classificationAndCheck(generations[i].queens))
-        {
-            printResult(generations[i].queens);
-            break;
-        }          
-    }    
+    return(a->cost > b->cost);
 }
 
-//select random genom by its score normalization
-genom selectRandom()
+chromosome* Generator()
 {
-    double persent = 0;
-    double rnd = rand() % 100;
-    int num = 0;
-    persent = 0;
-    qsort (numbers, QUEENS_NUMBER, sizeof(int), compare);
-    for (int j = QUEENS_NUMBER - 1; j >= 0; j--)
+    int randX, randY;
+    chromosome *childX, *childY, *child;
+    bool found =0;
+    while(!found)
     {
-        if(gensLength[j] != 0)
+        genom_type new_population;
+        for(unsigned int i = 0; i < population.size(); i++)
         {
-            persent += floor((numbers[j]/(double)totalNumber)*100) + 1;
-            if (rnd < persent)
+            generation++;
+            sort(population.begin(), population.end(), comp);
+
+            randX = randomSelection();
+            childX = population[randX];
+
+            randY =randomSelection();
+            childY = population[randY];
+
+            child = reproduce(childX, childY);
+
+            if(rand() % mutationRate == 0)
+                child = mutate(child);
+
+            if(checkFit(child))
             {
-                num = rand() % gensLength[j];
-                return gens[j][num];
+                found = 1;
+                return child;
             }
+            if (generation % 100 == 0)
+            {
+                cout << "G[" << generation << "] : " << child->arrangement << endl;
+            }
+            
+            new_population.push_back(child);
         }
+        population = new_population;
     }
-    num = rand() % gensLength[numbers[7]];
-    return gens[numbers[7]][num];
+    return child;
 }
 
-genom crossOver(genom g1, genom g2)
+void initialize(int chessboardSize, int firstPopSize, int mutatRate, int showStep)
 {
-    crossOverCallNumber++;
-    genom newGenom;
-    if(crossOverCallNumber % 100 < mutationRate)
-    {
-        genom mutatedGenom = mutation();
-        for (int i = 0; i < QUEENS_NUMBER; i++)
-        {
-            g2.queens[i].x = mutatedGenom.queens[i].x;
-            g2.queens[i].y = mutatedGenom.queens[i].y;
-        }        
-    }
-    int randX = rand() % QUEENS_NUMBER;
-    int randY = rand() % QUEENS_NUMBER;
-    for (int i = 0; i < QUEENS_NUMBER; i++)
-    {
-        if(g1.queens[i].x < randX)
-        {
-            newGenom.queens[i].x = g2.queens[i].x;
-        }
-        else
-        {
-            newGenom.queens[i].x = g1.queens[i].x;
-        }
-        if(g1.queens[i].y < randY)
-        {
-            newGenom.queens[i].y = g2.queens[i].y;
-        }
-        else
-        {
-            newGenom.queens[i].y = g1.queens[i].y;
-        }
-    }
-    return newGenom;  
+    srand(time(0));
+    generation = 0;
+    initialPopSize = firstPopSize;
+    boardSize = chessboardSize;
+    mutationRate = mutatRate;
+    printStep = showStep;
 }
-
 
 int main()
 {
     auto start = chrono::steady_clock::now();
-
     srand((unsigned) time(0));
 
+    initialize(8, 30, 2, 100);
     generateFirstGeneration();
-
-    while (true)
-    {
-        generation++;
-
-        genom newG = selectRandom();
-        bool res = classificationAndCheck(newG.queens);
-
-        if(res)
-        {
-            cout << "DONE";
-            printResult(newG.queens);
-            break;
-        }
-        else if(generation % 1000000 == 0)
-        {
-            cout << "G[" << generation << "] : " << fitness(newG.queens) << endl;
-        }
-    }
-    
+    chromosome *solution = Generator();
+    string IsFit = checkFit(solution) == 1 ? "TRUE" : "FALSE";
+    cout << "\n===============================\n";
+    cout << "Result:\n" 
+        << ">>> IsFit: " << IsFit << endl
+        << ">>> Arrangment: " << solution->arrangement << endl;
 
     auto end = chrono::steady_clock::now();
     auto elapsed = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "Time: " << elapsed.count() << " microseconds" << endl;
+    cout << ">>> ExecutionTime: " << elapsed.count() << " us";
+    cout << "\n===============================\n";
     return 0;
 }
